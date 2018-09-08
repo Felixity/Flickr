846d2ad18643352121f76cb08d5006e6ddc8a590
Laura <laura@devs-mbp.attlocal.net>
2018-09-08 13:13:43 -0700

    initial revision

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Laura on 9/8/18.
+//  Copyright © 2018 Laura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
